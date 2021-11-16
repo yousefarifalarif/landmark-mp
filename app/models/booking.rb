e4c66503e577ae446e937203d09934d89c9ef36c
Katie Lin <katiekklht@gmail.com>
2021-11-16 14:00:43 +0000

    Add booking actions

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :landmark
   belongs_to :user
+  validates :start_date, :end_date, presence: true, availability: true
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    errors.add(:end_date, "must be after the start date") if start_date > end_date
+  end
 end
