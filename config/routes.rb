c610e7baacb2db6e3b559c040727b26e61dd2a9d
Ethan Hardwick <hardwick.ethan@outlook.com>
2021-11-16 12:20:00 +0000

    added the specific landmark details page without booking functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :landmarks, only: %i[index new create] do
+  resources :landmarks, only: %i[index new create show] do
     resources :bookings, only: %i[new create]
   end
 end
