dcf286d20bafba8837d34393778e9a7bc6300b2f
Ethan Hardwick <hardwick.ethan@outlook.com>
2021-11-18 13:00:56 +0000

    added the map integration on the landmakr detail page

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -2,8 +2,11 @@ class Landmark < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  geocoded_by :name
 
   validates :user, :name, :location, :description, :price_per_day, presence: true
+
+  after_validation :geocode, if: :will_save_change_to_name?
 
   def unavailable_dates
     bookings.pluck(:start_date, :end_date).map do |range|
