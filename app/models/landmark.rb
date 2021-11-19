9d5970a174433c1277457900b97d6dd429770b42
Ethan Hardwick <hardwick.ethan@outlook.com>
2021-11-19 14:53:13 +0000

    added reviews to all cards

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,6 +1,7 @@
 class Landmark < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
   has_one_attached :photo
   monetize :price_cents
   geocoded_by :address
